849aa1151ccbc3957f5eb6814bf95ecfce3cf053
Jordan Berlyn <jberlyn@gmail.com>
2026-08-13 22:46:56 +0000

    Bump to v1.0.4

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-var Version = "v1.0.3"
+var Version = "v1.0.4"
 
 func main() {
 	seedFlag := flag.Bool("seed", false, "Seed the database with an admin user")
